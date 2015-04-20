80a39020497fbd82204c339620793817652e376f
Amber Douglas <douglasamber3@gmail.com>
2015-04-20 12:01:20 -0700

    jQueryAssessment

---
+++ main.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+	$('div').click(function(){
+		alert("Hey you clicked on me.");
+	});
+});
+
+$(document).ready(function(){
+	$('#iwao').click(function(){
+		alert("Hey you clicked on me again.");
+	});
+});
